3c550c7ed5ca9d9fd488b3be3e33e459b81cd271
Fabi <frontfabi@gmail.com>
2020-11-14 14:47:44 -0300

    [ADD] Gatsby files :tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `On The Road Salvador`,
+    description: `Site para o evento mais top do mundo.`,
+    author: `@frontfabi`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
